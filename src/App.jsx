38986b71a46a68b75ca4d612cd19ab57c63cc243
Benny Ou <benny.ou@matchboxsolutions.co.za>
2022-07-04 15:34:11 +0200

    added a logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+var image_Name = require("./images/recruiter_buddy_logo_yellow.PNG");
 const App = () => {
   return (
     <div className="landing">
+      <img className="logo" alt = "recruiterBuddy_ Logo" src = {image_Name}></img>
       <h1>Welcome to _recruiterBuddyDAO</h1>
     </div>
   );
